898b0457e0f3ce0ddbed6a99a2caea12d9f59c79
Nisha Choudhary <87658589+nisha1365@users.noreply.github.com>
2022-10-18 14:19:41 +0530

    Add files via upload

---
+++ Section_69/forms.py
@@ -0,0 +1,9 @@
+## MORE CODE ABOVE
+
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    submit = SubmitField("Sign Me Up!")
+
+## MORE CODE BELOW
